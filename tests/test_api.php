26c00837ce246fab4ed2bbd6f4032a538f5f25ca
Parnell Springmeyer <ixmatus@gmail.com>
2013-07-12 12:24:06 -0500

    A few more echo tests

--- tests/test_api.php
+++ tests/test_api.php
@@ -3,9 +3,33 @@
 require_once('../Ranktracker.php');
 require_once('../KeywordResearch.php');
 
-//$rt = new Ranktracker('QK1SsrVurJ5gUEBgLX9VOerNGyF8pIVETe1mYoo26WxM1nPXqMP6q', '9A5ntv7SdPFOQgW');
-$kwd = new KeywordResearch('nlWgru0aZP2btxSz7zZ77uZuxBH/sOMa2A4sU0BsIaFdSbmcSHtQG', '9A5ntv7SdPFOQgW');
+$rt = new Ranktracker('koHBYmkd5WySho2ijIx4cu6C7nlorP5Z8WqXl8A9PcRJdaIMv/JaS', 'Jibt78OGZcAwHln');
 
-echo '<h1>Keywords</h1>';
-echo var_dump($kwd->fetch("oranciata"));
-echo '<br />';
+$data = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?>
+<pair>
+  <url>http://example.com</url>
+  <keyword>a keyword</keyword>
+  <locale>87505</locale>
+</pair>
+EOT;
+
+$data1 = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?>
+<pair>
+  <url>http://someurl.com</url>
+  <keyword>yet another keyword</keyword>
+</pair>
+EOT;
+
+//echo print_r($rt->get_quota());
+//echo print_r($rt->get_all("ranked"));
+//echo print_r($rt->get_all("unranked"));
+//echo print_r($rt->get_all());
+//echo print_r($rt->get(530440));
+//echo print_r($rt->count());
+//echo print_r($rt->checksum_all());
+//echo print_r($rt->exists(530440));
+//echo print_r($rt->timeline_exists(530442));
+//echo print_r($rt->create($data));
+echo print_r($rt->update(531135, $data1));
